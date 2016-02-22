868aa73a937e55d6e4724316143c70d7595e6d57
Carlos Lawton <blakgeek@gmail.com>
2016-02-22 12:47:37 -0500

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # cordova-plugin-app-name
-A Cordova plugin that simplifies giving apps names with spaces and special characters.
+I created this plugin to address the issue of spaces in the names of my Cordova apps. 
+ 
+## How does it work?
+It adds a after_prepare hook that changes the value of app_name in strings.xml for Android and updates the "Project Name" and 
+"Project Display Name" in the plist for iOS.
+
+## How do I install it?
+
+If you're like me and using [Cordova CLI](http://cordova.apache.org/):
+```
+cordova plugin add cordova-plugin-app-name --variable APP_NAME="Look <Ma> I Got Spaces and Stuff!"
+```
+
+or
+
+```
+phonegap local plugin add cordova-plugin-app-name --variable APP_NAME="Look <Ma> I Got Spaces and Stuff!"
+```
+
+## How do I use it?
+Um ... See above :)
+
+
+
+
---
+++ src/rename-app.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require("path");
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
+var builder = new xml2js.Builder({
+    xmldec: {
+        version: '1.0',
+        encoding: 'UTF-8'
+    }
+});
+
+module.exports = function (context) {
+
+    console.log('Attempting to set app name');
+
+    var projectRoot = context.opts.projectRoot;
+    var configPath = path.join(projectRoot, 'platforms', 'android', 'res', 'xml', 'config.xml');
+    var stringsPath = path.join(projectRoot, 'platforms', 'android', 'res', 'values', 'strings.xml');
+    var stringsXml, name;
+
+    // make sure the android config file exists
+    try {
+        fs.accessSync(configPath, fs.F_OK);
+    } catch(e) {
+        return;
+    }
+
+    name = getConfigParser(context, configPath).getPreference('AppName');
+
+    if (name) {
+        stringsXml = fs.readFileSync(stringsPath, 'UTF-8');
+        parser.parseString(stringsXml, function (err, data) {
+
+            data.resources.string.forEach(function (string) {
+
+                if (string.$.name === 'app_name') {
+
+                    console.log('Setting App Name: ', name);
+                    string._ = name;
+                }
+            });
+
+            fs.writeFile(stringsPath, builder.buildObject(data));
+
+        });
+    }
+};
+
+function getConfigParser(context, config) {
+    var semver = context.requireCordovaModule('semver');
+
+    if (semver.lt(context.opts.cordova.version, '5.4.0')) {
+        ConfigParser = context.requireCordovaModule('cordova-lib/src/ConfigParser/ConfigParser');
+    } else {
+        ConfigParser = context.requireCordovaModule('cordova-common/src/ConfigParser/ConfigParser');
+    }
+
+    return new ConfigParser(config);
+}
